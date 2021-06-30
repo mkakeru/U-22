ae131081373b663fc5308e7de7a19ab308ec9093
mamaredo_n <11kakeru30@gmail.com>
2021-06-30 10:10:51 +0900

    外側の空間を追加

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container grid grid-cols-8 gap-6">
+  <div class="box-border px-4 container grid md:grid-cols-8 grid-cols-4 gap-6">
     <div class="grid grid-cols-4 col-span-4 col-start-3 w-full gap-6">
       <Nuxt />
     </div>
