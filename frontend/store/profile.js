1cd3ecc5343ff2e2451a5660717755553954b79f
mamaredo_n <11kakeru30@gmail.com>
2021-07-02 11:21:21 +0900

    profileのstoreを作成

---
+++ frontend/store/profile.js
@@ -0,0 +1,19 @@
+const state = () => ({
+  accountType: '',
+  firstName: '',
+  lastName: '',
+  gender: '',
+})
+
+const getters = {}
+
+const mutations = {}
+
+const actions = {}
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions,
+}
