82c674bd792d98d3f8bb1e209f9a36eda83186de
makotty <hal.m.90215@gmail.com>
2021-07-14 15:42:55 +0900

    ユーザー詳細に関するAPI作成（エラー解決）

--- backend/app/controllers/api/v1/user_details_controller.rb
+++ backend/app/controllers/api/v1/user_details_controller.rb
@@ -1,40 +1,38 @@
 class Api::V1::UserDetailsController < ApplicationController
-	before_action :set_user_detail, only: [:show, :update]
+  before_action :set_user_detail, only: [:show, :update]
 
-	def index
-		user_details = User_detail.order(created_at: :desc)
-		render json: { status: 'SUCCESS', message: 'Loaded user_details', data: user_details }
-	end
+  def index
+    user_details = UserDetail.order(created_at: :desc)
+    render json: { status: 'SUCCESS', message: 'Loaded user_details', data: user_details }
+  end
 
-	def show
-		render json: { status: 'SUCCESS', message: 'Loaded the user_detail', data: @user_detail }
-	end
+  def show
+    render json: { status: 'SUCCESS', message: 'Loaded the user_detail', data: @user_detail }
+  end
 
-	def create
-		user_detail = User_detail.new(user_detail_params)
-		if user_detail.save
-			render json: { status: 'SUCCESS', data: user_detail }
-		else
-			render json: { status: 'ERROR', data: user_detail.errors }
-		end
-	end
+  def create
+    user_dertail = UserDetail.new(user_detail_params)
+    if user.save
+      render json: { status: 'SUCCESS', data: user_detail }
+    else
+      render json: { status: 'ERROR', data: user_detail.errors }
+    end
+  end
 
-	def update
-		if @user_detail.update(user_detail_params)
-			render json: { status: 'SUCCESS', message: 'Updated the user_detail', data: @user_detail }
-		else
-			render json: { status: 'SUCCESS', message: 'Not updated', data: @user_detail.errors }
-		end
-	end
+  def update
+    if @user_detail.update(user_detail_params)
+      render json: { status: 'SUCCESS', message: 'Updated the user_detail', data: @user_detail }
+    else
+      render json: { status: 'SUCCESS', message: 'Not updated', data: @user_detail.errors }
+    end
+  end
 
-	private
+  private
+    def set_user_detail
+      @user_detail = UserDetail.find(params[:id])
+    end
 
-	def set_user_detail
-		@user_detail = User_detail.find(params[:id])
-	end
-
-	def user_detail_params
-		params.permit(:user_detail_gender, :user_detail_stature, :user_detail_age, :user_detail_features, 
-									:user_detail_image_path, :user_detail_notification_flag)
-	end
+    def user_detail_params
+      params.permit(:user_id, :user_detail_gender, :user_detail_stature, :user_detail_age, :user_detail_features, :user_detail_image_path, :user_detail_notification_flag)
+    end
 end
