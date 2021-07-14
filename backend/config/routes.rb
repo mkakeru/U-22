7871059afa3c70e9b62221877c6e3e6983ffbadc
makotty <hal.m.90215@gmail.com>
2021-07-14 23:29:08 +0900

    ヘルプに関するAPI作成

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resources :user_details, only: %i[index create show update]
+      resources :helps,        only: %i[index create show update]
     end
   end
 end
