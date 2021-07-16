624da5f61d6588171f67180ead2d41ab6daedd19
Shima1114 <metoro1119@gmail.com>
2021-07-16 12:40:52 +0900

    icon付きボタンの作成

---
+++ frontend/components/AppIconButton/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <AppButton>
+    <img :src="src" :alt="alt" />
+    <slot></slot>
+  </AppButton>
+</template>
+
+<script>
+import AppButton from '@/components/AppButton'
+export default {
+  name: 'AppIconButton',
+  components: { AppButton },
+  props: {
+    src: {
+      type: String,
+      default: 'src',
+    },
+    alt: {
+      type: String,
+      default: 'alt',
+    },
+    background: {
+      type: String,
+      default: 'white',
+    },
+  },
+  computed: {
+    customClass() {
+      // 'bg-' + this.background
+      return `bg-${this.background}`
+    },
+  },
+  methods: {
+    handleClick() {
+      this.$emit('buttonClick')
+    },
+  },
+}
+</script>
