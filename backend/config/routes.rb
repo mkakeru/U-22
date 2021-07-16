684faa39d76fdbc422296f99e2237120db70d30d
makotty <hal.m.90215@gmail.com>
2021-07-16 13:12:17 +0900

    Revert "不要なコードの削除"
    This reverts commit 4eae4826f687106d7bf83e25f0d492c278f8db02.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resources :user_details, only: %i[index create show update]
-      resources :helps
+      resources :helps,        only: %i[index create show update destroy]
     end
   end
 end
