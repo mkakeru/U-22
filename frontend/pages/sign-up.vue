fe5a78a6bc408eb20425bf5fd5ac8fa6c1adeecf
mamaredo_n <11kakeru30@gmail.com>
2021-07-17 20:17:16 +0900

    sign-upページの作成

--- frontend/pages/sign-up.vue
+++ frontend/pages/sign-up.vue
@@ -1,7 +1,37 @@
 <template>
-  <div>sign-up</div>
+  <div class="col-span-4">
+    <AppInputForm
+      title="名前"
+      caption="※名前を入力してください"
+      placeholder="山田 太郎"
+    />
+    <AppRadioForm title="性別" caption="※性別を選んでください" />
+    <AppButton background="primary">利用者として登録</AppButton>
+    <AppIconButton background="button-secondary" :src="plusIcon">
+      項目を増やす
+    </AppIconButton>
+  </div>
 </template>
 
 <script>
-export default {}
+import AppButton from '@/components/AppButton'
+import AppInputForm from '@/components/AppInputForm'
+import AppIconButton from '@/components/AppIconButton'
+import AppRadioForm from '@/components/AppRadioForm'
+import PlusIcon from '@/assets/svg/plus-icon.svg'
+
+export default {
+  components: {
+    AppButton,
+    AppInputForm,
+    AppIconButton,
+    AppRadioForm,
+  },
+  layout: 'default',
+  data() {
+    return {
+      plusIcon: PlusIcon,
+    }
+  },
+}
 </script>
