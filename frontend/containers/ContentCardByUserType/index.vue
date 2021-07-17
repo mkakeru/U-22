85f5c4d57615d47cb7d87c9d1521abd26d2a4e83
mamaredo_n <11kakeru30@gmail.com>
2021-07-17 20:19:32 +0900

    AppContentCardから受け取るUserTypeごとのコンテンツカードを作成

---
+++ frontend/containers/ContentCardByUserType/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <template v-if="userType === 'client'">
+      <AppContentCard
+        :button-text="clientText"
+        content-color="primary"
+        @buttonClick="handleClick"
+      />
+    </template>
+
+    <template v-else-if="userType === 'helper'">
+      <AppContentCard
+        :button-text="helperText"
+        content-color="secondary"
+        @buttonClick="handleClick"
+      />
+    </template>
+  </div>
+</template>
+
+<script>
+import AppContentCard from '@/components/AppContentCard'
+
+export default {
+  name: 'ContentCardByUserType',
+  components: { AppContentCard },
+  props: {
+    userType: {
+      type: String,
+      default: 'client',
+      require: true,
+    },
+  },
+  data() {
+    return {
+      clientText: '利用者として始める',
+      helperText: 'ヘルパーとして始める',
+    }
+  },
+  methods: {
+    handleClick() {
+      // eslint-disable-next-line no-console
+      console.log('buttontest')
+    },
+  },
+}
+</script>
