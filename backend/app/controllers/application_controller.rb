d316c84d7e34dfe4202fd12d6834ecedd4a4a773
makotty <hal.m.90215@gmail.com>
2021-07-22 14:55:20 +0900

    CSRFに関する設定の追加

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
+  skip_before_action :verify_authenticity_token, if: :devise_controller?, raise: false
+  include DeviseTokenAuth::Concerns::SetUserByToken
 end
