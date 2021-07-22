69e1553d63f8ec921141a5656cf8b766d9a2c49d
makotty <hal.m.90215@gmail.com>
2021-07-22 14:43:25 +0900

    LINE API連携に伴うUserテーブルの変更

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,20 @@
-class User < ApplicationRecord
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :omniauthable,
+        #  :confirmable,
+        #  :recoverable,
+         :registerable,
+         :rememberable,
+         :trackable,
+         :validatable
+  include DeviseTokenAuth::Concerns::User
+  serialize :tokens
+
   has_one  :user_detail, dependent: :destroy
   has_many :helps, dependent: :destroy
   has_many :emergency_contacts, dependent: :destroy
-
 end
