4ebc8b29dc15ac19d8afc68ee88127b2b88055c9
makotty <hal.m.90215@gmail.com>
2021-07-22 14:49:13 +0900

    LINE API用のomniauthの設定の追加

---
+++ backend/config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :line, Rails.application.credentials.line[:LINE_LOGIN_CHANNEL_ID], Rails.application.credentials.line[:LINE_LOGIN_CHANNEL_SECRET]
+  OmniAuth.config.allowed_request_methods = [:get, :post]
+end
