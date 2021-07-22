e06fa7396a3c6ff616723e45aa9ee665ad356676
makotty <hal.m.90215@gmail.com>
2021-07-22 14:48:36 +0900

    OmniauthCallbacksコントローラの追加

---
+++ backend/app/controllers/api/v1/omniauth_callbacks_controller.rb
@@ -0,0 +1,2 @@
+class Api::V1::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
+end
---
+++ backend/test/controllers/api/v1/omniauth_callbacks_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Api::V1::OmniauthCallbacksControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
