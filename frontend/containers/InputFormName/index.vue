621fe561882fa199e36b1e6fa6ce10dcaab05890
mamaredo_d <11kakeru30@gmail.com>
2021-07-30 16:23:48 +0900

    v-modelでinputの値を更新できるように処理を追加

--- frontend/containers/InputFormName/index.vue
+++ frontend/containers/InputFormName/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <div></div>
+  <AppInputForm
+    v-model="inputName"
+    :title="title"
+    :caption="caption"
+    :placeholder="placeholder"
+  />
 </template>
 
 <script>
+import AppInputForm from '@/components/AppInputForm'
+
 export default {
   name: 'InputFormName',
+  components: {
+    AppInputForm,
+  },
+  data() {
+    return {
+      title: '名前',
+      caption: '※名前を入力してください',
+      placeholder: '山田 太郎',
+      inputName: '',
+    }
+  },
 }
 </script>
