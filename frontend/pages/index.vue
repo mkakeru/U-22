32684d3f00ac60045a92db73f5025dc5d6ae3e34
mamaredo_d <11kakeru30@gmail.com>
2021-07-30 12:18:18 +0900

    captionの追加

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="col-span-4">
-    <div>
+    <div class="pt-12 pb-8">
       <Title />
+      <p class="pt-2 text-2xl font-bold text-caption">
+        caption caption caption
+        <span class="block">caption</span>
+      </p>
     </div>
     <div class="mb-4">
       <ContentCardByUserType user-type="client" />
