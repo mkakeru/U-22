e8021f982c27d82730bee6d8826bb153dd627abe
mamaredo_d <11kakeru30@gmail.com>
2021-07-30 12:17:27 +0900

    propsに画像を追加、スタイリングの修正

--- frontend/components/AppTitle/index.vue
+++ frontend/components/AppTitle/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>{{ title }} AppTitle</h1>
+  <div class="flex items-center">
+    <div class="w-16 h-16 mr-3 rounded-full bg-white">
+      <img :src="img.src" :alt="img.alt" />
+    </div>
+    <h1 class="font-bold text-5xl text-primary">{{ title }}</h1>
+  </div>
 </template>
 
 <script>
@@ -9,6 +14,10 @@ export default {
     title: {
       type: String,
       default: '',
+    },
+    img: {
+      type: Object,
+      default: () => {},
     },
   },
 }
