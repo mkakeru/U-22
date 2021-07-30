2dc997ee4db732d36baf73c78d946fb47cee953b
meishimomura <mei.shimomura@icloud.com>
2021-07-31 01:14:33 +0900

    config/initializers/omniauth.rbの編集<エラー原因>

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :line, Rails.application.credentials.line[:LINE_LOGIN_CHANNEL_ID], Rails.application.credentials.line[:LINE_LOGIN_CHANNEL_SECRET]
+  require 'omniauth/strategies/line'
+
   OmniAuth.config.allowed_request_methods = [:get, :post]
   OmniAuth.config.silence_get_warning = true
-
 end
