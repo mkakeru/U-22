4cd0d811569bc8d98990fb38a3f944efce92ab3e
mamaredo_d <11kakeru30@gmail.com>
2021-07-31 22:33:54 +0900

    selectboxフォームの完成

--- frontend/components/AppSelectbox/index.vue
+++ frontend/components/AppSelectbox/index.vue
@@ -1,9 +1,62 @@
 <template>
-  <div></div>
+  <select
+    v-model="currentItem"
+    :class="[{ 'text-disable': currentItem.text === '未選択' }]"
+    class="
+      font-bold
+      text-base text-primary
+      px-4
+      h-12
+      appearance-none
+      border border-gray-400
+      focus:outline-none
+      rounded
+    "
+  >
+    <template v-for="selectItem in selectItems">
+      <option
+        :key="selectItem.id"
+        :value="selectItem"
+        :label="selectItem.text"
+        :disabled="selectItem.text === '未選択'"
+        :class="[{ 'text-disable': selectItem.text === '未選択' }]"
+      >
+        {{ selectItem.text }}
+      </option>
+    </template>
+  </select>
 </template>
 
 <script>
 export default {
   name: 'AppSelectbox',
+  model: {
+    prop: 'value',
+    event: 'change',
+  },
+  props: {
+    name: {
+      type: String,
+      default: '',
+    },
+    value: {
+      type: Object,
+      default: () => {},
+    },
+    selectItems: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    currentItem: {
+      get() {
+        return this.value
+      },
+      set(e) {
+        return this.$emit('change', e)
+      },
+    },
+  },
 }
 </script>
