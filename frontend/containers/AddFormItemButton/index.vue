5a5e7c27077fcafbb02f9fe8d803715d1786851f
mamaredo_d <11kakeru30@gmail.com>
2021-08-01 20:54:37 +0900

    フォームの項目を追加するボタンを作成

---
+++ frontend/containers/AddFormItemButton/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <AppIconButton
+    background="button-secondary"
+    alt="plus-icon"
+    :src="plusIcon"
+    @handle-click="addFormItem"
+  >
+    項目を増やす
+  </AppIconButton>
+</template>
+
+<script>
+import AppIconButton from '@/components/AppIconButton'
+import PlusIcon from '@/assets/svg/plus-icon.svg'
+
+export default {
+  name: 'AddFormItemButton',
+  components: {
+    AppIconButton,
+  },
+  data() {
+    return {
+      plusIcon: PlusIcon,
+    }
+  },
+  methods: {
+    addFormItem() {},
+  },
+}
+</script>
