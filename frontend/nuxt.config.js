e6aa84052870ae6f232ca06447b396673b12e720
mamaredo_d <11kakeru30@gmail.com>
2021-08-07 12:40:06 +0900

    wsClientをpluginに追加

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -24,7 +24,10 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/axios.js'],
+  plugins: [
+    '@/plugins/axios.js',
+    { src: '@/plugins/webSocket/wsClient.js', mode: 'client' },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
