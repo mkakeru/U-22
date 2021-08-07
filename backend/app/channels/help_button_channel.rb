5d1e5d6f6561fe37472f6c5a92023f17b7dc16e9
meishimomura <mei.shimomura@icloud.com>
2021-08-08 01:32:03 +0900

    HelpButtonチャネルを編集

--- backend/app/channels/help_button_channel.rb
+++ backend/app/channels/help_button_channel.rb
@@ -1,10 +1,6 @@
 class HelpButtonChannel < ApplicationCable::Channel
   def subscribed
-    if !params.empty? && current_user.is_helper?(params['user_helper_flag'])
-      stream_from "help_button_channel_#{params['user_helper_flag']}"
-    else
-      reject
-    end
+    stream_from "help_button_channel"
   end
 
   def unsubscribed
@@ -12,7 +8,8 @@ class HelpButtonChannel < ApplicationCable::Channel
   end
 
   def sendToHelper(data)
-    ActionCable.server.broadcast "help_button_channel_#{data.user_helper_flag}", data: data
+    ActionCable.server.broadcast("help_button_channel", data)
     #! SMS送信
   end
+
 end
