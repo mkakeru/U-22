b25ab705678d8052c95c740f825fab980c085ce6
mamaredo_d <11kakeru30@gmail.com>
2021-08-07 12:38:24 +0900

    WebSocket classの作成

--- frontend/repositories/WebSocket/index.js
+++ frontend/repositories/WebSocket/index.js
@@ -1,6 +1,4 @@
 import ActionCable from 'actioncable'
-import client from '@/store/client'
-import helper from '@/store/helper'
 
 // _____________________________________________________________________________
 //
@@ -8,34 +6,25 @@ export default class WebSocket {
   constructor(accountType) {
     this.accountType = accountType
     this.cable = ActionCable.createConsumer(
-      `ws://${process.env.RAILS_IP}:${process.env.RAILS_PORT}/cable`
+      `ws://${process.env.RAILS_IP || '127.0.0.1'}:${
+        process.env.RAILS_PORT || 3000
+      }/cable`
     )
-    this.channel = ''
   }
 
-  setClientChannel(channel) {
-    if (this.accountType !== 'client') return
-    this.channel = this.cable.subscriptions.create(channel, {
-      received: data => {
-        client.mutations.setMessage(data)
-      },
-    })
+  setChannel(channelData) {
+    // eslint-disable-next-line no-console
+    console.log(this.accountType, channelData)
+    // if (this.accountType === 'client' || this.accountType === 'helper') return
+    // eslint-disable-next-line no-console
+    console.log(this.cable)
+    return this.cable.subscriptions.create(channelData)
   }
 
-  setHelperChannel(channel) {
-    if (this.accountType !== 'helper') return
-    this.channel = this.cable.subscriptions.create(channel, {
-      received: data => {
-        helper.mutations.setMessage(data)
-      },
-    })
+  disconnectAction() {
+    // eslint-disable-next-line no-console
+    console.log(this.cable)
   }
-
-  // helpAction() {
-  //   this.channel = this.cable.subscriptions.create('HelpButtonChannel', {
-
-  //   })
-  // }
 }
 // _____________________________________________________________________________
 //
