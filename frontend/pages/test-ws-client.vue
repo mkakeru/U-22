700fa7a7e80b1da7b86a2da95c3bf52b2ce3a55c
mamaredo_d <11kakeru30@gmail.com>
2021-08-07 12:41:39 +0900

    websocketのテストページを作成

---
+++ frontend/pages/test-ws-client.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="col-span-4">
+    <div class="pt-10">
+      <AppSectionText>
+        this page is websocket tests for client user
+      </AppSectionText>
+    </div>
+    <div class="pt-10">
+      <AppButton background="button-primary" @buttonClick="handleClick">
+        CONNECT WEBSOCKET
+      </AppButton>
+    </div>
+    <div class="pt-6">
+      <AppButton background="button-secondary" @buttonClick="disconnectAction">
+        DISCONNECT WEBSOCKET
+      </AppButton>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppSectionText from '@/components/AppSectionText'
+import AppButton from '@/components/AppButton'
+
+export default {
+  components: {
+    AppSectionText,
+    AppButton,
+  },
+  layout: 'default',
+  computed: {
+    _userId() {
+      return this.$store.getters['user/userId']
+    },
+  },
+  methods: {
+    handleClick() {
+      this.$wsClient._setChannel(this._userId)
+    },
+    disconnectAction() {
+      this.$wsClient.socketClose()
+    },
+  },
+}
+</script>
