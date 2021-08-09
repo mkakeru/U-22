84c79ad453bc04f4fe01d552737c90801c19ac4b
meishimomura <mei.shimomura@icloud.com>
2021-08-09 13:50:53 +0900

    frontend/repositories/WebSocket/index.jsを編集

--- frontend/repositories/WebSocket/index.js
+++ frontend/repositories/WebSocket/index.js
@@ -8,17 +8,19 @@ export default class WebSocket {
     this.cable = ActionCable.createConsumer(`ws://127.0.0.1:3000/cable`)
   }
 
-  setChannel(channelData) {
+  setChannel() {
     // if (this.accountType === 'client' || this.accountType === 'helper') return
     // eslint-disable-next-line no-console
+    const channelData = { userId: 1, lat: 10, lng: 10 }
     console.log(this.accountType, channelData)
     // eslint-disable-next-line no-console
     console.log(this.cable)
+    console.log(channelData)
     this.helpButtonChannel = this.cable.subscriptions.create(
       'HelpButtonChannel',
       {
-        received: channelData => {
-          return alert('test!')
+        received: data => {
+          console.log('受信データ', data)
         },
       }
     )
