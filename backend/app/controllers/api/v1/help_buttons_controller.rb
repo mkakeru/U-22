2141d61ca4e24ea9fdf8f8ba2d5b017446bcf324
meishimomura <mei.shimomura@icloud.com>
2021-08-09 15:22:00 +0900

    beforeアクションとprivateメソッドの追加

--- backend/app/controllers/api/v1/help_buttons_controller.rb
+++ backend/app/controllers/api/v1/help_buttons_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::HelpButtonsController < ApplicationController
+  before_action :set_user
+  before_action :set_helper
+
+  def send_help_message
+
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:uid])
+  end
+
+  def set_helper
+    @helper = User.find(params[:helper_id])
+  end
+
 end
