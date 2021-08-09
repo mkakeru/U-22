f9a94ac58d11df065bba5f77a6f1d8619e5af64b
meishimomura <mei.shimomura@icloud.com>
2021-08-09 18:26:00 +0900

    ヘルプLINEメッセージ送信用APIの作成

--- backend/app/controllers/api/v1/help_buttons_controller.rb
+++ backend/app/controllers/api/v1/help_buttons_controller.rb
@@ -1,9 +1,21 @@
 class Api::V1::HelpButtonsController < ApplicationController
+  require 'line/bot'
   before_action :set_user
   before_action :set_helper
 
   def send_help_message
-
+    @user_detail = @user.user_detail
+    gender = @user_detail.user_detail_gender == 0 ? "その他" : @user_detail.user_detail_gender == 1 ? "女性" : "男性"
+    message = {
+      type: 'text',
+      text: "#{@user.name}さんが助けを求めています。\n性別:#{gender}"
+    }
+    response = client.push_message(@helper.uid, message)
+    if response.code == "200"
+      render json: { status: 'SUCCESS', message: 'Sent a help message', data: response }
+		else
+			render json: { status: 'ERROR', data: response }
+    end
   end
 
   private
@@ -14,6 +26,13 @@ class Api::V1::HelpButtonsController < ApplicationController
 
   def set_helper
     @helper = User.find(params[:helper_id])
+  end
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret  = Rails.application.credentials.line[:LINE_BOT_CHANNEL_SECRET]
+      config.channel_token = Rails.application.credentials.line[:LINE_BOT_ACCESS_TOKEN]
+    }
   end
 
 end
