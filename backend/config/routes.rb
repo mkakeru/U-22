6166350798365c0c9a8c23e49f2dedbcb88c9e99
meishimomura <mei.shimomura@icloud.com>
2021-08-11 01:13:57 +0900

    resources :usersを削除

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -7,9 +7,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         :omniauth_callbacks => "api/v1/omniauth_callbacks",
-        :registrations => 'api/v1/registrations'
+        :registrations => "api/v1/registrations"
       }, via: [:get, :post]
-      resources :users
       resources :user_details, only: %i[index create show update]
       resources :helps
       resources :emergency_contacts
