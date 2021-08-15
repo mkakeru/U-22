9a4c339bb817417992a0587ab8855dd3e9a605c7
mamaredo_d <11kakeru30@gmail.com>
2021-08-15 17:18:40 +0900

    repositoryのプログラムをplugin/websocketへ移動

--- frontend/plugins/webSocket/wsClient.js
+++
@@ -1,6 +0,0 @@
-import ClientRepository from '@/repositories/WebSocket/clientRepository'
-
-export default function ({ store }, inject) {
-  const wsClient = new ClientRepository(store)
-  inject('wsClient', wsClient)
-}
