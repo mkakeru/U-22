9d3d96ad66f7290a6468664f1b72573541476294
mamaredo_d <11kakeru30@gmail.com>
2021-08-16 18:16:11 +0900

    profileの写真を追加する機能を追加

---
+++ frontend/components/AppPictureForm/AddPictureButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <AppIconButton
+    :src="photoIcon"
+    alt="photo-icon"
+    background="button-secondary"
+    @buttonClick="buttonClick"
+  >
+    写真を選ぶ
+    <input
+      ref="input"
+      class="hidden"
+      type="file"
+      accept="image/jpeg, image/png"
+      @change="selectedFile"
+    />
+  </AppIconButton>
+</template>
+
+<script>
+import AppIconButton from '@/components/AppIconButton'
+import PhotoIcon from '@/assets/svg/photo-icon.svg'
+
+export default {
+  name: 'AddPictureButton',
+  components: {
+    AppIconButton
+  },
+  data() {
+    return {
+      photoIcon: PhotoIcon,
+      uploadedImage: ''
+    }
+  },
+  methods: {
+    buttonClick() {
+      this.$refs.input.click()
+    },
+    selectedFile() {
+      const file = this.$refs.input.files[0]
+      this.$emit('selectedFile', file)
+    }
+  }
+}
+</script>
---
+++ frontend/components/AppPictureForm/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <AppForm title="写真の追加" caption="※写真を1枚追加できます">
+    <div class="mt-6">
+      <p class="font-medium">
+        いつも身に着けているものや <br />
+        介護道具の写真を追加してください
+      </p>
+    </div>
+
+    <div class="mt-4">
+      <AddPictureButton @selectedFile="selectedFile" />
+    </div>
+
+    <div
+      class="mt-4 aspect-w-4 aspect-h-3 rounded-md"
+      :class="{ 'bg-disable': !uploadedPhoto }"
+    >
+      <img
+        v-show="uploadedPhoto"
+        class="object-contain"
+        :src="uploadedPhoto"
+        alt=""
+      />
+    </div>
+  </AppForm>
+</template>
+
+<script>
+import AppForm from '@/components/AppForm'
+import AddPictureButton from './AddPictureButton'
+
+export default {
+  name: 'AppPictureForm',
+  components: {
+    AppForm,
+    AddPictureButton
+  },
+  props: {
+    uploadedPhoto: {
+      type: null,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      supplementaryText: `
+      いつも身に着けているものや
+      介護道具の写真を追加してください
+      `
+    }
+  },
+  computed: {
+    _uploadedPhoto() {
+      return this.uploadedPhoto
+    }
+  },
+  methods: {
+    selectedFile(file) {
+      this.$emit('selectedFile', file)
+    }
+  }
+}
+</script>
