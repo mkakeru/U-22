8e590ac9a82d9318217b7de86b7005fefbfdc18f
mamaredo_d <11kakeru30@gmail.com>
2021-08-23 17:48:49 +0900

    add: responsive style

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="box-border px-4 container grid md:grid-cols-8 grid-cols-4 gap-6">
-    <div class="grid grid-cols-4 col-span-4 col-start-3 w-full gap-6">
+  <div
+    class="
+      box-border
+      px-4
+      container
+      grid
+      md:grid-cols-8
+      grid-cols-4
+      md:gap-6
+      gap-4
+    "
+  >
+    <div
+      class="grid grid-cols-4 col-span-4 md:col-start-3 w-full md:gap-6 gap-4"
+    >
       <Nuxt />
     </div>
   </div>
