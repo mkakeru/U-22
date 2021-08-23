cdb54ab389d4153ccbd96541c8fb9fa15e98b9a7
mamaredo_d <11kakeru30@gmail.com>
2021-08-23 17:41:12 +0900

    add: check input value method

--- frontend/store/user/index.js
+++ frontend/store/user/index.js
@@ -1,8 +1,8 @@
 // _____________________________________________________________________________
 //
-const state = () => ({
-  userId: 'user-id',
-  accountType: '',
+const initState = {
+  // userId: 'user-id',
+  // accountType: '',
   userName: '',
   gender: { id: 0, text: 'その他' },
   age: { id: 6, text: '未選択' },
@@ -10,10 +10,24 @@ const state = () => ({
   helpList: [''],
   emergencyNameList: [''],
   emergencyTelList: [''],
-  emergencyContactList: [{ name: '', tel: '' }],
+  featureList: ['']
+}
+const state = () => ({
+  userId: 'user-id',
+  accountType: '',
+  requiredState: { ...initState },
+  userName: '',
+  gender: { id: 0, text: 'その他' },
+  age: { id: 6, text: '未選択' },
+  height: { id: null, text: '未選択' },
+  helpList: [''],
+  emergencyNameList: [''],
+  emergencyTelList: [''],
   featureList: [''],
+  emergencyContactList: [{ name: '', tel: '' }],
   featurePhoto: '',
-  carrierWavePhoto: ''
+  carrierWavePhoto: '',
+  isComplete: { bool: false, errVal: '' }
 })
 // _____________________________________________________________________________
 //
@@ -32,6 +46,7 @@ export { STATE_TYPES_OF_USER }
 // _____________________________________________________________________________
 //
 const getters = {
+  requiredState: state => state.requiredState,
   userId: state => state.userId,
   accountType: state => state.accountType,
   userName: state => state.userName,
@@ -41,10 +56,19 @@ const getters = {
   helpList: state => state.helpList,
   emergencyNameList: state => state.emergencyNameList,
   emergencyTelList: state => state.emergencyTelList,
-  emergencyContactList: state => state.emergencyContactList,
+  emergencyListLength: (state, getters) => getters.emergencyContactList.length,
   featureList: state => state.featureList,
   featurePhoto: state => state.featurePhoto,
-  carrierWaveFormat: state => state.carrierWaveFormat
+  carrierWaveFormat: state => state.carrierWaveFormat,
+  isComplete: state => state.isComplete,
+  selectedValue: (state, getters) => {
+    return {
+      gender: getters.gender.text,
+      age: getters.age.text,
+      height: getters.height.text
+    }
+  },
+  emergencyContactList: state => state.emergencyContactList
 }
 // _____________________________________________________________________________
 //
@@ -56,19 +80,23 @@ const mutations = {
       state.accountType = type
     }
   },
+
   setInputValue(state, inputValue) {
     state.userName = inputValue
   },
+
   setInputValues(state, { type, index, payload }) {
     if (
       type === STATE_TYPES_OF_USER.helpList ||
-      type === STATE_TYPES_OF_USER.emergencyContactList ||
+      type === STATE_TYPES_OF_USER.emergencyNameList ||
+      type === STATE_TYPES_OF_USER.emergencyTelList ||
       type === STATE_TYPES_OF_USER.featureList
     ) {
       // state[`${type}`] = [...state[`${type}`], { ...payload }]
       state[`${type}`][index] = payload
     }
   },
+
   setSelectedItem(state, { type, payload }) {
     if (
       type === STATE_TYPES_OF_USER.gender ||
@@ -78,9 +106,39 @@ const mutations = {
       state[`${type}`] = { ...payload }
     }
   },
+
   setFeaturePhoto(state, payload) {
     state.featurePhoto = payload.encodedData
     state.carrierWaveFormat = payload.carrierWave
+  },
+
+  checkIsCompleate(state) {
+    const _isComplete = {
+      bool: false,
+      errVal: ''
+    }
+    _isComplete.bool = Object.keys(state.requiredState).every(val => {
+      const initValue =
+        state.requiredState[val].text || state.requiredState[val][0]
+      const currentValue = state[val].text || state[val][0]
+      _isComplete.errVal = initValue !== currentValue ? '' : val
+      return initValue !== currentValue
+    })
+    state.isComplete = _isComplete
+  },
+
+  mergeEmergencyList(state) {
+    const mergencyList = state.emergencyNameList.map((el, i) => {
+      return {
+        tel: state.emergencyTelList[i],
+        name: el
+      }
+    })
+    state.emergencyContactList = mergencyList
+  },
+
+  addListCount(state, { type }) {
+    state[type] = [...state[type], '']
   }
 }
 // _____________________________________________________________________________
