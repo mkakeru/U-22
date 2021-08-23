2e31b604871d645e828c6d02b69b5dca52c08f7f
mamaredo_d <11kakeru30@gmail.com>
2021-08-23 17:44:11 +0900

    create: hitokoe main page

---
+++ frontend/pages/hitokoe/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hitokoe</div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $api, store }) {
+    const auth = store.getters['authLine/auth']
+
+    const { data } = await $api.user.getUserInfomation(auth.uid)
+    // eslint-disable-next-line no-console
+    console.log(data)
+  }
+}
+</script>
