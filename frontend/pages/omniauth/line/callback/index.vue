d0e9a42bf42eb6dcbf78b6ad45db5a1728df26b1
mamaredo_d <11kakeru30@gmail.com>
2021-08-23 17:45:04 +0900

    create: call back page

---
+++ frontend/pages/omniauth/line/callback/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>callback content</div>
+</template>
+
+<script>
+export default {
+  middleware: ['serverRedirect'],
+  asyncData({ redirect }) {
+    redirect(301, '/hitokoe')
+  }
+}
+</script>
