67574aff0d5e01fae8e8564e530c0e7a852b9e6d
mamaredo_d <11kakeru30@gmail.com>
2021-08-26 23:37:04 +0900

    create: sos page

---
+++ frontend/containers/ContentCardBySos/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <AppContentCard
+      button-text="助けを呼ぶ"
+      content-color="primary"
+      @buttonClick="dialogShow"
+    />
+    <p class="pt-2 font-bold text-secondary">※注意書きテキスト</p>
+    <div :class="{ hidden: dialogFlag }">
+      <AppDialog @dialogClose="dialogClose" />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppContentCard from '@/components/AppContentCard'
+import AppDialog from '@/components/AppDialog'
+
+export default {
+  name: 'ContentCardBySos',
+  components: {
+    AppContentCard,
+    AppDialog
+  },
+  data() {
+    return {
+      dialogFlag: true
+    }
+  },
+  methods: {
+    dialogShow() {
+      this.dialogFlag = false
+    },
+    dialogClose() {
+      this.dialogFlag = true
+    }
+  }
+}
+</script>
