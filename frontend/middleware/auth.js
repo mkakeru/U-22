cd90de32c45656245c55b01b7d26672ac490f83d
mamaredo_d <11kakeru30@gmail.com>
2021-08-26 23:32:27 +0900

    fix: auth middleware

--- frontend/middleware/auth.js
+++ frontend/middleware/auth.js
@@ -1,4 +1,5 @@
-export default async function ({ app, redirect }) {
+export default async function ({ app, redirect, route }) {
+  if (route.path === '/' || route.path === '/omniauth/line/callback') return
   const session = await app.$cookies.get('session')
-  if (!session) return redirect('/')
+  if (!session) return redirect(302, '/')
 }
