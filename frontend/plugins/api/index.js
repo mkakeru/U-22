8436839d46cff55676d4b278e0bc59e1d8b93da9
mamaredo_d <11kakeru30@gmail.com>
2021-08-30 01:42:50 +0900

    update: modularize axios

--- frontend/plugins/api/index.js
+++ frontend/plugins/api/index.js
@@ -1,35 +1,17 @@
 import user from './repositories/user'
+import helper from './repositories/helper'
 import geolocation from './repositories/geolocation'
 
 export default function (context, inject) {
-  const { $axios, store } = context
+  const { $userAxios, $geoAxios, store } = context
 
-  // _____________________________________________________________________________
-  //
-  const userAxios = $axios.create()
-  userAxios.setBaseURL(`https://${process.env.RAILS_DOMAIN}`)
-  userAxios.onRequest(config => {
-    const headers = store.getters['authLine/auth']
-    config.headers = {
-      'Access-Token': headers['access-token'],
-      client: headers.client,
-      uid: headers.uid
-    }
-  })
-  // _____________________________________________________________________________
-  //
-  // _____________________________________________________________________________
-  //
-  const geoAxios = $axios.create()
-  geoAxios.setBaseURL(
-    `https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.GEO_API_KEY}`
-  )
-  // _____________________________________________________________________________
-  //
+  const version = '/api/v1'
   const api = {
-    user: user(userAxios, store),
-    geolocation: geolocation(geoAxios, store)
+    user: user($userAxios, store, version),
+    helper: helper($userAxios, store, version),
+    geolocation: geolocation($geoAxios, store)
   }
+
   context.$api = api
   context.$user = api.user
   inject('api', api)
