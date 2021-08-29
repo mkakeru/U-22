212f9946ad4d7d195be074c98d5648ca180f4ead
mamaredo_d <11kakeru30@gmail.com>
2021-08-30 01:39:12 +0900

    update: consolidate common items

--- frontend/plugins/webSocket/repositories/WsClient.js
+++ frontend/plugins/webSocket/repositories/WsClient.js
@@ -1,74 +1,65 @@
 import webSocket from '../repositories'
 
 export default function (store) {
-  const clientSocket = webSocket('client')
   const state = {
-    channel: null,
     receiveValue: null,
+    isHelper: false,
     resource: 'HelpButtonChannel'
   }
-  const wsClient = new WsClient(clientSocket, store, state)
+  const clientSocket = webSocket(state.isHelper, state.resource)
+  const wsClient = new WsClient(clientSocket, store, state.receiveValue)
   return wsClient
 }
 // _____________________________________________________________________________
 //
 class WsClient {
-  constructor(webSocket, store, state) {
+  constructor(webSocket, store, receiveValue) {
     this.webSocket = webSocket
     this.store = store
-    this.channel = state.channel
-    this.receiveValue = state.receiveValue
-    this.resource = state.resource
+    this.receiveValue = receiveValue
   }
 
   clientChannelLink() {
-    this.webSocket.reconnectAction()
-    if (this.channel !== null) return
-    // eslint-disable-next-line no-console
-    console.log('setChannel')
-    this.channel = this.webSocket.cable.subscriptions.create(
-      {
-        channel: this.resource
+    this.webSocket.channelLink({
+      connected() {
+        // eslint-disable-next-line no-console
+        return console.log('nice to meet you webSocket from client')
       },
-      {
-        connected() {
-          // eslint-disable-next-line no-console
-          return console.log('nice to meet you webSocket')
-        },
-        received: data => {
-          return this._received(data)
-        }
+      received: data => {
+        return this._received(data)
       }
-    )
+    })
   }
 
-  _received({ data }) {
+  _received(res) {
+    const id = this.store.getters['user/userId']
+    if (res.data.is_helper === true || res.data.clientId !== id) return
     // eslint-disable-next-line no-console
     return console.log(`
-    prop type ${typeof data}
-    ${data}
-    ${data.userId}
-    ${data.lat}
-    ${data.lng}
+    prop type ${typeof res.data}
+    ${res.data}
+    is_helper = ${res.data.is_helper}
+    userId = ${res.data.clientId}
+    lat = ${res.data.lat}
+    lng = ${res.data.lng}
     `)
   }
 
-  _sendToHelper(_userId) {
-    if (this.channel === null) return
-    const lat = this.store.getters['userPosition/lat']
-    const lng = this.store.getters['userPosition/lng']
-    return this.channel.perform('sendToHelper', {
-      data: { userId: _userId, lat, lng }
+  _sendToHelper(_lat, _lng) {
+    if (this.webSocket.channel === null || this.webSocket.isHelper) return
+    const userId = this.store.getters['user/userId']
+    return this.webSocket.channel.perform('sendToHelper', {
+      data: {
+        clientId: userId,
+        lat: _lat,
+        lng: _lng,
+        is_helper: this.webSocket.isHelper
+      }
     })
   }
 
   _disconnectAction() {
-    if (this.channel === null) return
-    this.channel.unsubscribe()
-    this.channel = null
-    // eslint-disable-next-line no-console
-    console.log(this.channel)
-    this.webSocket.disconnectAction()
+    this.webSocket.disconnectSocket()
   }
 }
 // _____________________________________________________________________________
