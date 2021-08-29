f2a1a369be0ec3c82c1a9ad872c8cd2bfe4ef02d
mamaredo_d <11kakeru30@gmail.com>
2021-08-29 14:51:43 +0900

    add: account delete button

--- frontend/pages/hitokoe/setting/index.vue
+++ frontend/pages/hitokoe/setting/index.vue
@@ -1,9 +1,30 @@
 <template>
-  <div>setting</div>
+  <div>
+    <div class="pt-8 mb-4">
+      <AppSectionText>設定</AppSectionText>
+    </div>
+    <div>
+      <AppButton background="button-primary" @buttonClick="deleteAccount">
+        アカウント削除
+      </AppButton>
+    </div>
+  </div>
 </template>
 
 <script>
+import AppSectionText from '@/components/AppSectionText'
+import AppButton from '@/components/AppButton'
+
 export default {
-  layout: 'main'
+  components: {
+    AppSectionText,
+    AppButton
+  },
+  layout: 'main',
+  methods: {
+    async deleteAccount() {
+      await this.$api.user.deleteAccount()
+    }
+  }
 }
 </script>
