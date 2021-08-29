ae8aed912a0d020ab8be9ba9810ff2ee1bc34db1
mamaredo_d <11kakeru30@gmail.com>
2021-08-30 03:51:10 +0900

    route.pathを正規表現で判断するように更新

--- frontend/middleware/auth.js
+++ frontend/middleware/auth.js
@@ -1,11 +1,12 @@
 export default async function ({ app, redirect, route }) {
-  if (route.name === 'index') {
-    const isHelper = localStorage.getItem('is_helper')
-    if (isHelper !== null) {
-      return isHelper === 'true' ? redirect('/helper') : redirect('/hitokoe')
-    }
-  }
   if (route.path === '/' || route.path === '/omniauth/line/callback') return
   const session = await app.$cookies.get('session')
   if (!session) return redirect(302, '/')
+
+  const isHelper = localStorage.getItem('is_helper')
+  if (/^hitokoe-?/.test(route.name) && isHelper === 'true') {
+    redirect('/helper')
+  } else if (/^helper-?/.test(route.name) && isHelper === 'false') {
+    redirect('/hitokoe')
+  }
 }
