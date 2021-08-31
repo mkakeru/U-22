1f03e73dbc67d6c1c991a9fba8f1f3dd28072093
mamaredo_d <11kakeru30@gmail.com>
2021-08-31 18:27:21 +0900

    layoutの更新

--- frontend/pages/hitokoe/setting/index.vue
+++ frontend/pages/hitokoe/setting/index.vue
@@ -4,23 +4,35 @@
       <AppSectionText>設定</AppSectionText>
     </div>
     <div>
-      <AppButton background="button-primary" @buttonClick="deleteAccount">
-        アカウント削除
-      </AppButton>
+      <AppForm
+        title="アカウント削除"
+        caption="※削除されたアカウントは復元できません"
+        :is-required="false"
+      >
+        <AppButton
+          background="button-secondary"
+          :is-full-width="false"
+          @buttonClick="deleteAccount"
+        >
+          <span class="px-6"> 削除する </span>
+        </AppButton>
+      </AppForm>
     </div>
   </div>
 </template>
 
 <script>
 import AppSectionText from '@/components/AppSectionText'
+import AppForm from '@/components/AppForm'
 import AppButton from '@/components/AppButton'
 
 export default {
   components: {
     AppSectionText,
+    AppForm,
     AppButton
   },
-  layout: 'main',
+  layout: 'hitokoe',
   methods: {
     async deleteAccount() {
       await this.$api.user.deleteAccount()
