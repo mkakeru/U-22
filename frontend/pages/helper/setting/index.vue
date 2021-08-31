801c620debebad987d455da547bcc79764e55813
mamaredo_d <11kakeru30@gmail.com>
2021-08-31 18:26:58 +0900

    helper page helper/setting pageを更新

---
+++ frontend/pages/helper/setting/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="pt-8 mb-4">
+      <AppSectionText>設定</AppSectionText>
+    </div>
+    <div>
+      <AppForm
+        title="アカウント削除"
+        caption="※削除されたアカウントは復元できません"
+        :is-required="false"
+      >
+        <AppButton
+          background="button-secondary"
+          :is-full-width="false"
+          @buttonClick="deleteAccount"
+        >
+          <span class="px-6">削除する</span>
+        </AppButton>
+      </AppForm>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppSectionText from '@/components/AppSectionText'
+import AppForm from '@/components/AppForm'
+import AppButton from '@/components/AppButton'
+
+export default {
+  components: {
+    AppSectionText,
+    AppForm,
+    AppButton
+  },
+  layout: 'helper'
+}
+</script>
